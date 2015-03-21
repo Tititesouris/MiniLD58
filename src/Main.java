de46b798847f9d21057844c3abe9825561a85454
Quentin Brault <quentin.brault@etudiant.univ-lille1.fr>
2015-03-21 10:20:57 +0100

    Here we go!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main is a test class.
+ *
+ * @author Tititesouris
+ * @version 0.0.0
+ * @since 2015-03-21
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
